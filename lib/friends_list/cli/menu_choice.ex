56d00d67e67005aa7875564f3d326894df8789df
Gabriel Sugai <gabrielsugai@gmail.com>
2025-09-04 19:57:49 -0300

    Possibilita escolha de opcoes do menu

--- lib/friends_list/cli/menu_choice.ex
+++ lib/friends_list/cli/menu_choice.ex
@@ -5,9 +5,17 @@ defmodule FriendsList.CLI.MenuChoice do
     Shell.cmd("clear")
     Shell.info("Choose an option:")
 
-    FriendsList.CLI.MenuItens.all()
+    menu_itens = FriendsList.CLI.MenuItens.all()
+    find_menu_item_by_index = &Enum.at(menu_itens, &1) # Declaracao de funcao anonima que escolhe um item da lista
+
+    menu_itens
     |> Enum.map(&(&1.label)) # Pipe operator + funcao anonima reduzida
-    |> display_options
+    |> display_options()
+    |> generate_question()
+    |> Shell.prompt
+    |> parse_answer()
+    |> find_menu_item_by_index.()
+    |> confirm_menu_item()
   end
 
   defp display_options(options) do
@@ -16,5 +24,28 @@ defmodule FriendsList.CLI.MenuChoice do
     |> Enum.each(fn {option, index} ->
       Shell.info("#{index} - #{option}") # Funcao anonima convencional
     end)
+
+    options # Retorna o options para que o retorno da funcao seja utilizado com o pipe operator para a proxima funcao
+  end
+
+  defp generate_question(options) do
+    options = Enum.join(1..Enum.count(options), ", ")
+    "Choose one of the above options: [#{options}]\n"
+  end
+
+  defp parse_answer(answer) do
+    {option, _} = Integer.parse(answer) # Utiliza o pattern match para atribuir a opcao ao option (a funcao parse retorna o resultado e um texto)
+    option - 1
+  end
+
+  defp confirm_menu_item(chosen_menu_item) do
+    Shell.cmd("clear")
+    Shell.info("You chose: #{chosen_menu_item.label}\n")
+
+    if Shell.yes?("Is that correct? [y/n]") do
+      Shell.info("... #{chosen_menu_item.label} ...\n")
+    else
+      start()
+    end
   end
 end
