017eacbbc2f63a5d768a210c6925f05a83b70031
Gabriel Sugai <gabrielsugai@gmail.com>
2025-09-03 23:29:57 -0300

    cria primeira task

--- lib/friends_list.ex
+++ lib/friends_list.ex
@@ -1,17 +1,4 @@
 defmodule FriendsList do
-  @moduledoc """
-  Documentation for `FriendsList`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FriendsList.hello()
-      :world
-
-  """
   def hello do
     :world
   end
---
+++ lib/mix/tasks/start.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.Start do
+  use Mix.Task # Modulo padrao do elixir que utiliza a funcao run para executar a task
+
+  # Para que a task apareca no mix help e necessario que adc essa descricao
+  @shortdoc "Start [Friends List App]"
+  def run(_) do
+    IO.puts("Staring...")
+  end
+end
