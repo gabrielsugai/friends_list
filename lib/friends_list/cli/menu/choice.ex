3a0eb1d675b392c4c507d949fd9efc55c581fa37
Gabriel Sugai <gabrielsugai@gmail.com>
2025-09-11 19:37:55 -0300

    desfaz alteracoes

--- lib/friends_list/cli/menu/choice.ex
+++ lib/friends_list/cli/menu/choice.ex
@@ -7,18 +7,15 @@ defmodule FriendsList.CLI.Menu.Choice do
     Shell.info("Choose an option:")
 
     menu_itens = Itens.all()
-    # find_menu_item_by_index = &Enum.at(menu_itens, &1, :error) # Declaracao de funcao anonima que escolhe um item da lista / o terceiro parametro eh o valor default caso o index nao exista na lista
+    find_menu_item_by_index = &Enum.at(menu_itens, &1, :error) # Declaracao de funcao anonima que escolhe um item da lista / o terceiro parametro eh o valor default caso o index nao exista na lista
 
     menu_itens
     |> Enum.map(&(&1.label)) # Pipe operator + funcao anonima reduzida
     |> display_options()
     |> generate_question()
     |> Shell.prompt
-    |> prepare_input()
     |> parse_answer()
-    |> IO.inspect(label: "Parsed answer")
-    |> find_menu_item_by_index()
-    # |> IO.inspect(label: "Chosen menu item")
+    |> find_menu_item_by_index.()
     |> confirm_menu_item()
     |> confirm_menu_item_flow()
   end
@@ -33,32 +30,16 @@ defmodule FriendsList.CLI.Menu.Choice do
     options # Retorna o options para que o retorno da funcao seja utilizado com o pipe operator para a proxima funcao
   end
 
-  defp prepare_input(input) do
+  defp parse_answer(input) do
     case Integer.parse(input) do
-      :error -> :error
+      :error -> invalid_input()
       {option, _} -> option
-    end
-  end
-  
-  defp find_menu_item_by_index(index) do
-    case index do
-      :error -> :error
-      :ok -> nil
-      _ -> Enum.at(Itens.all(), index, :error)
     end
   end
 
   defp generate_question(options) do
     options = Enum.join(1..Enum.count(options), ", ")
     "Choose one of the above options: [#{options}]\n"
-  end
-
-  defp parse_answer(answer) when is_integer(answer) do
-    answer - 1
-  end
-
-  defp parse_answer(_) do
-    invalid_input()
   end
   
   defp invalid_input do
